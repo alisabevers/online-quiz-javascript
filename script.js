5fd4e96d2cc7506684c83ea1bd0d3397729710c5
A Bevers <alisabevers@gmail.com>
2023-04-07 20:00:46 -0400

    created HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,22 @@
+var timeEl = document.querySelector(".time");
+var timesUpEl = document.querySelector("#timesUp");
+var secondsLeft = 60;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+// function endQuiz () {
+//     timeEl.textContent = " ";
+
+// }
+
+setTime();
